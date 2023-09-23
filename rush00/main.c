a7045739d08858b8b8726fede43decf6d6760ea5
Rômulo Nunes <romulotg21@gmail.com>
2023-09-23 13:08:52 -0300

    Rush as Insane Project but we did it!

---
+++ rush00/main.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+void	rush(int x, int y);
+
+int	main(void)
+{
+	rush(123, 42);
+	return (0);
+}
---
+++ rush00/rush00.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	print_char(int x, char schar, char mchar, char echar)
+{
+	ft_putchar(schar);
+	while ((x - 1) > 1)
+	{
+		ft_putchar(mchar);
+		x--;
+	}
+	if (x > 1)
+		ft_putchar(echar);
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	if (x > 0 && y > 0)
+	{
+		print_char(x, 'o', '-', 'o');
+		y--;
+		while (y > 1)
+		{
+			print_char(x, '|', ' ', '|');
+			y--;
+		}
+		if (y > 0)
+			print_char(x, 'o', '-', 'o');
+	}
+}
